14bf5d434dfa986ee1f7685eae3abeea3cc6cb58
Raafay hussain <mudassar76642@gmail.com>
2022-06-21 02:00:03 -0700

    test

--- app/src/main/java/com/example/fitnessapplication/signup.java
+++ app/src/main/java/com/example/fitnessapplication/signup.java
@@ -11,6 +11,7 @@ public class signup extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         //kjskjja
-        //,mnm
+        //
+        //
     }
 }
