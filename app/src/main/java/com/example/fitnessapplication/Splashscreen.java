6af5ad39acec368027009c123cbdf38848751fa3
Namra Habib <namiihabib987@gmail.com>
2022-06-21 22:45:02 -0700

    Splashscreen image is added+ changes is done for bottom navigation names+splashscreen java code and xml is done

--- app/src/main/java/com/example/fitnessapplication/Splashscreen.java
+++ app/src/main/java/com/example/fitnessapplication/Splashscreen.java
@@ -2,7 +2,9 @@ package com.example.fitnessapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splashscreen extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class Splashscreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+        getSupportActionBar().hide();
+
+        int secondsDelayed = 2;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(Splashscreen.this, MainActivity.class));
+                finish();
+            }
+        }, secondsDelayed * 1000);
     }
 }
