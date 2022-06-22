43426e6c7621deca4db2851f2d3873a035fa4e72
Raafay hussain <mudassar76642@gmail.com>
2022-06-22 01:19:02 -0700

    signup library

--- app/src/main/java/com/example/fitnessapplication/signup.java
+++ app/src/main/java/com/example/fitnessapplication/signup.java
@@ -3,13 +3,44 @@ package com.example.fitnessapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.hbb20.CountryCodePicker;
 
 public class signup extends AppCompatActivity {
 
+    EditText phonnoE;
+    Button Register;
+    CountryCodePicker ccodeV;
+    String countryCode,phoneNumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        getSupportActionBar().hide();
+        phonnoE = findViewById(R.id.phonnov);
+        ccodeV  = findViewById(R.id.ccp);
+        Register = findViewById(R.id.btnRegister);
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                countryCode = ccodeV.getSelectedCountryCode();
+                phoneNumber = phonnoE.getText().toString();
+                Toast.makeText(signup.this, ""+countryCode+phoneNumber, Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+
+
+
 
     }
+
+
+
 }
