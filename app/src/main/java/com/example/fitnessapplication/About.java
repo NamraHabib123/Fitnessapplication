c4ab07604546c96a7a34ff3769caf075b90dc1ba
Namra Habib <namiihabib987@gmail.com>
2022-07-02 21:40:21 -0700

    extra scrren are deleted

--- app/src/main/java/com/example/fitnessapplication/About.java
+++ app/src/main/java/com/example/fitnessapplication/About.java
@@ -4,12 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MaleWorkoutscreen extends AppCompatActivity {
+public class About extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_male_workoutscreen);
+        setContentView(R.layout.activity_about);
         getSupportActionBar().hide();
     }
 }
